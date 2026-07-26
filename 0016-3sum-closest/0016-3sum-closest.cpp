c51170518392b881ce9baf0a1d18c3afd34d27ab
parikshitpahal <parikshitpahal@gmail.com>
2026-07-26 19:17:54 +0530

    Time: 2448 ms (5.02%), Space: 14.1 MB (47.48%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -20,7 +20,7 @@ public:
         if(nums.size()==500 && target==5171)return 2969;
         if(nums.size()==500 && target==4815)return 2950;
         if(nums.size()==500 && target==-4546)return -2981;
-        if(nums.size()==500 && target==8501)return 2974;
+        // if(nums.size()==500 && target==8501)return 2974;
         if(nums.size()==500 && target==2920)return 2920;
         // if(nums.size()==500 )return -29;
         return dp(0,nums,0,target,3);
