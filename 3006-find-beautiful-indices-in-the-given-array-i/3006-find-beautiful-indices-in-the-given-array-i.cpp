e80dccfe83b2b3d315f7061bffb3b8eb18c320bb
parikshitpahal <parikshitpahal@gmail.com>
2026-09-14 19:39:27 +0530

    Time: 527 ms (5.27%), Space: 18.8 MB (78.09%) - LeetHub

---
+++ 3006-find-beautiful-indices-in-the-given-array-i/3006-find-beautiful-indices-in-the-given-array-i.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    vector<int> beautifulIndices(string s, string a, string b, int k) {
+        vector<int>aa;
+        vector<int>bb;
+        int idx=0;
+        for(int i=0;i<s.size();i++){
+            idx=i;
+            int c=0;
+            while(c<a.size() && s[i]==a[c] && i<s.size()){
+                i++;
+                c++;
+            }
+
+            if(c==a.size() && c!=0){
+                aa.push_back(idx);
+                cout<<idx<<" ";
+            }
+            
+            i=idx;
+            
+        }
+        cout<<endl;
+
+        for(int i=0;i<s.size();i++){
+            idx=i;
+            int c=0;
+            while(c<b.size() && s[i]==b[c] && i<s.size()){
+                i++;
+                c++;
+            }
+            if(c==b.size() && c!=0){
+                bb.push_back(idx);
+                cout<<idx<<" ";
+            }
+            
+            i=idx;
+            
+        }
+
+        sort(aa.begin(),aa.end());
+        sort(bb.begin(),bb.end());
+        vector<int>ams;
+
+        for(int i=0;i<aa.size();i++){
+            for(int j=0;j<bb.size();j++){
+                if(abs(aa[i]-bb[j])<=k){
+                   
+                    ams.push_back(aa[i]);
+                    break;
+                }
+            }
+        }
+        if(aa.size()==0 || bb.size()==0)return {};
+        if(ams.size()>aa.size())ams.resize(aa.size());
+        return ams;
+    }
+};
