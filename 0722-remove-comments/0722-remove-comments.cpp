fe74c5f6b47282478c4d40f90722a11a856ec9f6
parikshitpahal <parikshitpahal@gmail.com>
2026-09-17 19:27:45 +0530

    Time: 0 ms (100%), Space: 10.5 MB (46.75%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.cpp
+++ 0722-remove-comments/0722-remove-comments.cpp
@@ -1,58 +1,44 @@
+
 class Solution {
 public:
     vector<string> removeComments(vector<string>& source) {
-         
-        if(source[0]=="/*/dadb/*/aec*////*//*ee*//*//b*////*badbda//*bbacdbbd*//ceb//*cdd//**//de*////*"){return {"aec*","ec","ddadbede","e","eed*","bab","c/bb*","cbae*","dcabebdc","badcc","dd*","eb/dcdbaaadd","ba*","ab","*","*","aeabdccccd","c/aa","de/aedb*","*","*","/dc","e/edceacc/ea*","ca","ec","ebdce","dadc","eadddaabebeedd","cbeadebcaebded*","ee","eb","dd","cbccc","da*","d*","b*","dac*","de","e","b","dbbbe","ccd*","*","adaabdaaea","eec*","/a/addc","*","*","/ddddcab*","cb*","b*","*","aaadddd","bd*","ad","*","*","e","a*","a","d*","e*","cedc*","*","*","eb","*","b*","*","ba*","da*","eccd*/ab","*","*/cbcedae*","a","aa","*","cadbbd","d","*","c","d/d","d/c","dbbdedece"};}
-        if(source[0]=="//*cdacbbad/*/ccae//*d*//ebaec*////*/*/d*//de//*a//*c/*//*/c/*/ec/*/dbdd//*add"){
-            return {"ab","db","bb","cadadca*","aaded/b","d*","c*","e/aeaaeca","cd","cb*","adcbcdaa/cd","eca","cbc/be","*","ececcbaca/bdab*","*","e/bacbd","b","e","/dab","*","aee*","dcedde","aeddda*","a*","adac*","*","ecbd*","*","b","b","da","e/cac*","*","eaa","ea*","c","bad/e","aeea","*","be","/a*","aabeeac/aae*","db","b/ececc","caaebed*","/beee","bce","*","*","c","dd","a","eb*","*","dace*","edaeeb","aac*","ccdce*","ccc","b","*","*","aecaacabe","/d*","ddbe","c*","c*","c","c*/ebbbbc*","dc","*","*","c*","/a*","*","ba*"};
-        }
-        vector<string>ans;
-        int flag=0;
-        string tmp="";
-        for(auto it:source){
-            char pre=it[0];
-            if(flag==0 && pre!='/')tmp=pre;
-            else if(flag==0)tmp="";
-            int idx=1;
-            if(flag==0){ 
-                for(int i=1;i<it.size();i++){
-                    if(pre=='/'&& it[i]=='/'){
-                        if(tmp.size()>1)tmp.pop_back();
+        vector<string> ans;
+        string tmp = "";
+        bool inBlock = false;
+
+        for (const string& line : source) {
+            int n = line.size();
+            for (int i = 0; i < n; ++i) {
+                if (inBlock) {
+  
+                    if (i + 1 < n && line[i] == '*' && line[i + 1] == '/') {
+                        inBlock = false;
+                        i++; 
+                    }
+                } else {
+
+                    if (i + 1 < n && line[i] == '/' && line[i + 1] == '/') {
                         break;
                     }
-                    if(pre=='/'&& it[i]=='*'){
-                        flag=1;
-                        if(tmp.size()>1)tmp.pop_back();
-                        
-                        pre=it[idx];
-                        if(idx<it.size())pre=it[idx];
-                        idx=i+1;
-                        break;
+                    
+                    else if (i + 1 < n && line[i] == '/' && line[i + 1] == '*') {
+                        inBlock = true;
+                        i++;
+                    } 
+
+                    else {
+                        tmp += line[i];
                     }
-                    pre=it[i];
-                    tmp+=it[i];
                 }
             }
-            if(flag==1){
-                
-                for(int i=idx;i<it.size();i++){
-                    if(pre=='*' && it[i]=='/' && flag==1){
-                        flag=0; 
-                        // pre=it[i];
-                        continue;
-                    }
-                    if(pre=='/' && it[i]=='/' && flag==0){
-                        tmp.pop_back();
-                        break;
-                    }
-                    if(flag==0){
-                        tmp+=it[i];
-                    }
-                    pre=it[i];
-                }
+
+          
+            if (!inBlock && !tmp.empty()) {
+                ans.push_back(tmp);
+                tmp = ""; 
             }
-            if(flag==0 && tmp.size()>=1)ans.push_back(tmp);
         }
+
         return ans;
     }
 };
